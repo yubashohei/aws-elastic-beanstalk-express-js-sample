6ede1a5632472e756abf58d9ee54a0ffd3d2754d
yuba <yuba1192@yahoo.co.jp>
2025-11-06 20:56:58 +0900

    こんにちはに変更

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Good bye!'));
+app.get('/', (req, res) => res.send('こんにちは'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
